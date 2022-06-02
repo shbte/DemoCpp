613cae3cdf8462355849996a3ea680dfe416fe52
shbte <530278469@qq.com>
2022-06-02 16:50:21 +0800

    int: 析构函数、静态成员变量、静态成员函数

---
+++ src/module03/05/module0305.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+
+/*
+==>
+const关键字能够修饰对象
+const修饰的对象为只读对象
+只读对象的成员变量不允许被改变
+只读对象是编译阶段的概念，运行时无效
+
+C++中的const成员函数
+    const对象只能调用const的成员函数
+    const成员函数中只能调用const成员函数
+    const成员函数中不能直接改写成员变量的值
+
+所有的对象共享类的成员函数，隐藏的this指针用于表示当前对象
+<==
+*/
+class A
+{
+private:
+    int m_i = 0;
+    long m_l = 100;
+
+public:
+    A();
+    A(int i);
+    A(const A &a);
+    int getMI() const; // 函数的声明和定义要一起家const
+    long getML();
+    void setMI(int i) const;
+    A *printThis();
+    ~A();
+};
+
+A::A()
+{
+    printf("A()\n");
+}
+
+A::A(int i)
+{
+    m_i = i;
+    printf("A(int i):i=%d\n", i);
+}
+
+A::A(const A &a)
+{
+    printf("A(const A& a)\n");
+}
+
+int A::getMI() const // 函数的声明和定义要一起家const
+{
+    return m_i;
+}
+
+long A::getML()
+{
+    return m_l;
+}
+
+void A::setMI(int i) const
+{
+    // const成员函数中不能直接改写成员变量的值
+    // m_i = i; // 表达式必须是可修改的左值
+
+    // 但可以间接改变
+    int *p = const_cast<int *>(&m_i);
+    *p = i;
+}
+
+A *A::printThis()
+{
+    return this;
+}
+
+A::~A()
+{
+    printf("~A():m_i=%d\n", m_i);
+}
+
+long fun()
+{
+    return 0;
+}
+
+int main()
+{
+
+    const A a(4);
+    // const对象只能调用const的成员函数
+    printf("a.getMI()=%d\n", a.getMI());
+    // const成员函数中只能调用const成员函数
+    // printf("a.getML()=%ld\n", a.getML()); // 对象含有与成员 函数 "A::getML" 不兼容的类型限定符,对象类型是: const A
+
+    a.setMI(11);
+    printf("a.setMI(11) => a.getMI()=%d\n", a.getMI());
+
+    A a1(1);
+    A a2(2);
+    A a3(3);
+
+    // 所有的对象共享类的成员函数，隐藏的this指针用于表示当前对象
+    printf("a.getMI():m_i=%d, this=%p, &a1=%p, &A::printThis=%p\n", a1.getMI(), a1.printThis(), &a1, (void *)(A * (A::*)()) & A::printThis);
+    printf("a.getMI():m_i=%d, this=%p, &a2=%p, &A::printThis=%p\n", a2.getMI(), a2.printThis(), &a2, (void *)&A::printThis);
+    printf("a.getMI():m_i=%d, this=%p, &a3=%p, &A::printThis=%p\n", a3.getMI(), a3.printThis(), &a3, (void *)&A::printThis);
+
+    printf("fun_addr=%p\n", &fun);
+
+    return 0;
+}
---
+++ src/module03/06/module0306.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+
+/*
+==>
+                    静态成员函数    普通成员函数
+所有对象共享            Yes             Yes
+隐含this指针            No             Yes
+访问普通成员变量(函数)    No             Yes
+访问静态成员变量(函数)   Yes             Yes
+通过类名直接调用         Yes             No
+通过对象名直接调用       Yes             Yes
+<==
+*/
+class A
+{
+private:
+    int m_i = 0;
+    long m_l = 10;
+    static int s_i; // 静态成员变量不可以在类内进行初始化
+
+public:
+    A(int i);
+    static int s_pi;
+    void setMI(int i);
+    int getMI();
+    static void setML(A& a, long l);
+    int getML();
+    void setSI(int i);
+    int getSI();
+    ~A();
+};
+
+int A::s_i = 2; // 静态成员变量赋值
+int A::s_pi = 3;
+
+A::A(int i)
+{
+    m_i = i;
+    std::cout << "A(int i):m_i=" << m_i << std::endl;
+}
+
+void A::setMI(int i)
+{
+    m_i = i;
+}
+
+int A::getMI()
+{
+    return m_i;
+}
+
+void A::setML(A& a, long l)
+{
+    // m_i; // 静态成员函数不能访问普通成员变量
+    a.m_i; // 通过对象，静态成员函数可以访问普通成员变量
+
+    a.m_l = l;
+}
+
+int A::getML()
+{
+    return m_l;
+}
+
+void A::setSI(int i)
+{
+    s_i = i;
+}
+
+int A::getSI()
+{
+    return s_i;
+}
+
+A::~A()
+{
+    std::cout << "~A()" << std::endl;
+}
+
+int main()
+{
+    A a(3);
+    std::cout << "m_i=" << a.getMI() << std::endl;
+    a.setMI(5);
+    std::cout << "m_i=" << a.getMI() << std::endl;
+
+    std::cout << "m_l=" << a.getML() << std::endl;
+    a.setML(a, 15);
+    std::cout << "m_l=" << a.getML() << std::endl;
+    // 静态成员函数可直接通过类名进行访问
+    A::setML(a, 20);
+    std::cout << "m_l=" << a.getML() << std::endl;
+
+    std::cout << "s_i=" << a.getSI() << std::endl;
+    a.setSI(30);
+    std::cout << "s_i=" << a.getSI() << std::endl;
+
+    // 静态成员变量可直接通过类名进行访问
+    std::cout << "s_pi=" << A::s_pi << std::endl;
+
+    return 0;
+}
